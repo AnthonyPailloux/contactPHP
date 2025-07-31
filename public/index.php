df351acc90bae92d995d9da58d3f60bbfdd807bc
AnthonyPailloux <anthonypailloux@outlook.com>
2025-07-31 15:30:43 +0200

    contactPHP2

--- public/index.php
+++ public/index.php
@@ -14,6 +14,26 @@
     <?php include "../includes/header.php" ;?>
     <main>
 
+        <form action="" method="POST"></form>
+        <ul>
+            <li>
+                <label for="pseudo">Pseudo :</label>
+                <input type="text" id="pseudo" placeholder="entrte ton pseudo">
+            </li>
+
+            <li>
+                <label for="message">Message :</label>
+                <input type="text" id="message" placeholder="entre ton message">
+            </li>
+
+            <div>
+                <input type="submit" value="Valider">
+            </div>
+        </ul>
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST")
+        ?>
+
     </main>
     <!-- deuxieme methode -->
     <?php require "../includes/footer.php" ;?>
