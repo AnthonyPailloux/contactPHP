5905b4b7306309f624b46a5f719e07379753473b
AnthonyPailloux <anthonypailloux@outlook.com>
2025-07-31 15:01:38 +0200

    contactPHP1

---
+++ includes/footer.php
@@ -0,0 +1,1 @@
+<footer>super !</footer>
---
+++ includes/header.php
@@ -0,0 +1,10 @@
+<header>
+    <h1>mon header</h1>
+    <nav>
+        <ul>
+            <li>
+                <a href=""></a>
+            </li>
+        </ul>
+    </nav>
+</header>
